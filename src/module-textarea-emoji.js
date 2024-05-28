08cb48ae656507e07d0a324521de8b2d3beaf9b2
tobyealden <toby.ealden@gmail.com>
2024-05-28 07:10:38 +0100

    switch off prettier

--- src/module-textarea-emoji.js
+++ src/module-textarea-emoji.js
@@ -1,13 +1,13 @@
-import Quill from "quill";
-import Fuse from "fuse.js";
-import emojiList from "./emoji-list.js";
+import Quill from 'quill';
+import Fuse from 'fuse.js';
+import emojiList from './emoji-list.js';
 
-const Delta = Quill.import("delta");
-const Module = Quill.import("core/module");
+const Delta = Quill.import('delta');
+const Module = Quill.import('core/module');
 
 class TextAreaEmoji extends Module {
-  constructor(quill, options) {
-    super(quill, options);
+    constructor(quill, options){
+        super(quill, options);
 
     this.quill = quill;
     this.container = document.createElement("div");
@@ -109,78 +109,74 @@ class TextAreaEmoji extends Module {
       }
       fn_emojiPanelInit(panel, this.quill);
     }
-  }
 }
 
 TextAreaEmoji.DEFAULTS = {
-  buttonIcon:
-    '<svg viewbox="0 0 18 18"><circle class="ql-fill" cx="7" cy="7" r="1"></circle><circle class="ql-fill" cx="11" cy="7" r="1"></circle><path class="ql-stroke" d="M7,10a2,2,0,0,0,4,0H7Z"></path><circle class="ql-stroke" cx="9" cy="9" r="6"></circle></svg>',
-};
-
-function fn_close() {
-  let ele_emoji_plate = document.getElementById("textarea-emoji");
-  document.getElementById("emoji-close-div").style.display = "none";
-  if (ele_emoji_plate) {
-    ele_emoji_plate.remove();
-  }
+  buttonIcon: '<svg viewbox="0 0 18 18"><circle class="ql-fill" cx="7" cy="7" r="1"></circle><circle class="ql-fill" cx="11" cy="7" r="1"></circle><path class="ql-stroke" d="M7,10a2,2,0,0,0,4,0H7Z"></path><circle class="ql-stroke" cx="9" cy="9" r="6"></circle></svg>'
 }
 
-function fn_updateRange(quill) {
-  let range = quill.getSelection();
-  return range;
+function fn_close(){
+    let ele_emoji_plate = document.getElementById('textarea-emoji');
+    document.getElementById('emoji-close-div').style.display = "none";
+    if (ele_emoji_plate) {ele_emoji_plate.remove()}
 }
 
-function fn_emojiPanelInit(panel, quill) {
-  fn_emojiElementsToPanel("p", panel, quill);
-  document.querySelector(".filter-people").classList.add("active");
+function fn_updateRange(quill){
+    let range = quill.getSelection();
+    return range;
 }
 
-function fn_emojiElementsToPanel(type, panel, quill) {
-  let fuseOptions = {
-    shouldSort: true,
-    matchAllTokens: true,
-    threshold: 0.3,
-    location: 0,
-    distance: 100,
-    maxPatternLength: 32,
-    minMatchCharLength: 3,
-    keys: ["category"],
-  };
-  let fuse = new Fuse(emojiList, fuseOptions);
-  let result = fuse.search(type);
-  let results = result
-    .map((r) => r.item)
-    .sort(function (a, b) {
+function fn_emojiPanelInit(panel,quill){
+    fn_emojiElementsToPanel('p', panel, quill);
+    document.querySelector('.filter-people').classList.add('active');
+}
+
+function fn_emojiElementsToPanel(type,panel,quill){
+    let fuseOptions = {
+                    shouldSort: true,
+                    matchAllTokens: true,
+                    threshold: 0.3,
+                    location: 0,
+                    distance: 100,
+                    maxPatternLength: 32,
+                    minMatchCharLength: 3,
+                    keys: [
+                        "category"
+                    ]
+                };
+    let fuse = new Fuse(emojiList, fuseOptions);
+    let result = fuse.search(type);
+    let results = result.map(r => r.item).sort(function (a, b) {
       return a.emoji_order - b.emoji_order;
     });
 
-  quill.focus();
-  let range = fn_updateRange(quill);
+    quill.focus();
+    let range = fn_updateRange(quill);
 
-  results.map(function (emoji) {
-    let span = document.createElement("span");
-    let t = document.createTextNode(emoji.shortname);
-    span.appendChild(t);
-    span.classList.add("bem");
-    span.classList.add("bem-" + emoji.name);
-    span.classList.add("ap");
-    span.classList.add("ap-" + emoji.name);
-    let output = "" + emoji.code_decimal + "";
-    span.innerHTML = output + " ";
-    panel.appendChild(span);
+    results.map(function(emoji) {
+        let span = document.createElement('span');
+        let t = document.createTextNode(emoji.shortname);
+        span.appendChild(t);
+        span.classList.add('bem');
+        span.classList.add('bem-' + emoji.name);
+        span.classList.add('ap');
+        span.classList.add('ap-'+emoji.name);
+        let output = ''+emoji.code_decimal+'';
+        span.innerHTML = output + ' ';
+        panel.appendChild(span);
 
-    let customButton = document.querySelector(".bem-" + emoji.name);
-    if (customButton) {
-      customButton.addEventListener("click", function () {
-        // quill.insertText(range.index, customButton.innerHTML);
-        // quill.setSelection(range.index + customButton.innerHTML.length, 0);
-        // range.index = range.index + customButton.innerHTML.length;
-        quill.insertEmbed(range.index, "emoji", emoji, Quill.sources.USER);
-        setTimeout(() => quill.setSelection(range.index + 1), 0);
-        fn_close();
-      });
-    }
-  });
+        let customButton = document.querySelector('.bem-' + emoji.name);
+        if (customButton) {
+            customButton.addEventListener('click', function() {
+                // quill.insertText(range.index, customButton.innerHTML);
+                // quill.setSelection(range.index + customButton.innerHTML.length, 0);
+                // range.index = range.index + customButton.innerHTML.length;
+                quill.insertEmbed(range.index, 'emoji', emoji, Quill.sources.USER);
+                setTimeout(() => quill.setSelection(range.index + 1), 0);
+                fn_close();
+            });
+        }
+    });
 }
 
 export default TextAreaEmoji;
